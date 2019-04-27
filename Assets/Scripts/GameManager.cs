b86bb128c6a884161acdbeda13b26d1eabcb200c
Karel Kahula <kkahula@gmail.com>
2019-04-27 18:21:01 -0500

    some stuff for processing coin judgements

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+    [Range(5, 200)]
+    public int TargetSum = 100;
+    [Range(0, 1)]
+    public float MaxHealthPoints = 1f;
+    public float HealthPoints;
+
+    public float SuccessReward = 0.25f;
+    public float FailPenalty = 0.15f;
+
+    private enum GameState {
+        Evaluating,
+        EvaluationComplete,
+        SuccessfulRound,
+        FailedRound,
+        Waiting
+    }
+
+    private GameState gameState;
+    private int CurrentSum = 0;
+    private int CoinCounter = 0;
+    private bool DummyCoinAccepted = false;
+
+    // Start is called before the first frame update
+    void Start() {
+    }
+
+    // Update is called once per frame
+    void Update() {
+        switch (gameState) {
+            case (GameState.Evaluating):
+                if (CoinCounter == 0) {
+                    Debug.Log("no coins left");
+                    gameState = GameState.EvaluationComplete;
+                    CheckEvaulation();
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void CoinAdded() {
+        CoinCounter++;
+    }
+
+    public void CoinAccepted(CoinConfig config) {
+        Debug.Log($"CoinAccepted {config.Amount}");
+        CoinCounter--;
+        CurrentSum += config.Amount;
+        DummyCoinAccepted = DummyCoinAccepted || config.IsDummy;
+    }
+
+    public void CoinRejected(CoinConfig config) {
+        Debug.Log($"CoinRejected {config.Amount}");
+        CoinCounter--;
+    }
+
+    private void CheckEvaulation() {
+        // what if we accepted a dummy coin?
+        // do dummy coins have analagous value?
+        if(CurrentSum == TargetSum) {
+            HealthPoints = Mathf.Min(HealthPoints + SuccessReward, MaxHealthPoints);
+        }
+        else {
+            HealthPoints = Mathf.Max(HealthPoints - FailPenalty, 0);
+        }
+        Debug.Log($"Health Points: {HealthPoints}");
+    }
+}
