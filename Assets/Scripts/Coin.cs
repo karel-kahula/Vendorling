313c9c57198dda42f0a7ea08d50246ada1de205a
Karel Kahula <kkahula@gmail.com>
2019-04-27 13:48:33 -0500

    initial commit

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    private Vector2 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown() {
+        var screenPoint = Camera.main.WorldToScreenPoint(transform.position);
+        offset = transform.position - Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    private void OnMouseDrag() {
+        var screenPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = new Vector2(screenPos.x, screenPos.y) + offset;
+    }
+}
---
+++ Assets/Scripts/MouseController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseController : MonoBehaviour {
+    private GameObject selectedCoin;
+    private Vector2 mouseDown;
+    private Vector2 mouseUp;
+    private DateTime mouseDownTime;
+    private DateTime mouseUpTime;
+
+    [Range(0,1)]
+    public float swipeDistanceMin = 0.3f;
+    [Range(0,2)]
+    public float swipeTimeMax = 1f;
+
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        var cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        if (Input.GetMouseButtonDown(0)) {
+            mouseDown = new Vector2(cursorPos.x, cursorPos.y);
+            mouseDownTime = DateTime.UtcNow;
+            var hit = Physics2D.Raycast(mouseDown, Vector2.zero, 10f);
+            selectedCoin = hit ? hit.transform.gameObject : null;
+        }
+        if (Input.GetMouseButtonUp(0)) {
+            if(selectedCoin) {
+                mouseUp = new Vector2(cursorPos.x, cursorPos.y);
+                mouseUpTime = DateTime.UtcNow;
+                var swipeDeltaX = mouseUp.x - mouseDown.x;
+                var swipeDeltaTime = (float)mouseUpTime.Subtract(mouseDownTime).TotalSeconds;   
+
+                if(swipeTimeMax > swipeDeltaTime && swipeDistanceMin < Mathf.Abs(swipeDeltaX)) {
+                    if(swipeDeltaX > 0) {
+                        Debug.Log($"right {swipeDeltaX}");
+                    }
+                    else {
+                        Debug.Log($"left {swipeDeltaX}");
+                    }
+                    selectedCoin = null;
+                }
+            }
+        }
+    }
+}
